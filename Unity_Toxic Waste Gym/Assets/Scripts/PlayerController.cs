f0eeccc867a25bd8e76a37f83e82a97ecc7d18b2
anavc <ana.v.carrasco97@gmail.com>
2024-01-10 02:33:23 +0900

    Added sprites for players, robot, ball and terrain. Added script for player movement

---
+++ Unity_Toxic Waste Gym/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    public bool isMoving;
+
+    public Vector2 input;
+
+}
