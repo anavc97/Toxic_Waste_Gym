ebca911faae45cee10a9a6ceceb792ab3d3b78ed
rodrigo1110 <rodrigogoma007@gmail.com>
2024-01-10 16:47:51 +0000

    Old map update

--- Unity_Toxic Waste Gym/Assets/Scripts/Human_Movement.cs
+++ Unity_Toxic Waste Gym/Assets/Scripts/Human_Movement.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class Human_Movement : MonoBehaviour
 {
-    [SerializeField] float movementSpeed;
+    public float movementSpeed;
+    public Transform movePoint;
+    /*[SerializeField] float movementSpeed;
     float vertical, horizontal;
-    Rigidbody2D myRigidbody2D;
+    Rigidbody2D myRigidbody2D;*/
 
     // Start is called before the first frame update
     void Start()
     {
-      myRigidbody2D = GetComponent<Rigidbody2D>();  
+      movePoint.parent = null;
+      //myRigidbody2D = GetComponent<Rigidbody2D>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
+      transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementSpeed * Time.deltaTime);
+
+      if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+      {
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+        {
+          movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        }
+        else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+        {
+          movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        }
+         
+      }
+        /*horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-        myRigidbody2D.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+        if (horizontal != 0) vertical = 0;
+        myRigidbody2D.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);*/
     }
 }
