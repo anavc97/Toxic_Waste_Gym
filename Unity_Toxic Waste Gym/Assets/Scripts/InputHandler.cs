c1dc95592ae2724e8a38162bce4b6454da41a5c9
rodrigo1110 <rodrigogoma007@gmail.com>
2024-01-22 16:05:33 +0000

    Input handling implementation

--- Unity_Toxic Waste Gym/Assets/Scripts/InputHandler.cs
+++ Unity_Toxic Waste Gym/Assets/Scripts/InputHandler.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 //using Newtonsoft.Json;
 
 
-/*public class Action
-{
-    public Vector2 move;
-    public int Grab;
-}*/
-
-
 public class NewBehaviourScript : MonoBehaviour
 {
-    //private List<Action> action = new List<Action>();
+    [System.Serializable]
+    public class Action
+    {
+        public Vector2 Move; //Movement position 
+        public int Grab; //1 if player has clicked space, 0 otherwise
+    }
+    
+    private string filePath =  Application.dataPath + "/human_action.json";
     // Start is called before the first frame update
     void Start()
     {
@@ -24,20 +24,38 @@ public class NewBehaviourScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-       /* if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+    {       
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
         {
-            action.Add(new Action(new Vector2(Input.GetAxisRaw("Horizontal"),0),0));
+            Action action = new Action
+            {
+                Move = new Vector2(Input.GetAxisRaw("Horizontal"),0),
+                Grab = 0
+            };
+            string jsonString = JsonUtility.ToJson(action);
+            System.IO.File.WriteAllText(filePath, jsonString);
         }
         else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
         {
-            action.Add(new Action(new Vector2(0,Input.GetAxisRaw("Vertical")),0));
+            Action action = new Action
+            {
+                Move = new Vector2(0,Input.GetAxisRaw("Vertical")),
+                Grab = 0
+            };
+            string jsonString = JsonUtility.ToJson(action);
+            System.IO.File.WriteAllText(filePath, jsonString);
         }
         else if(Input.GetKeyDown(KeyCode.Space))
         {
-            action.Add(new Action(new Vector2(0,0),1));
+            Action action = new Action
+            {
+                Move = new Vector2(0,0),
+                Grab = 1
+            };
+            string jsonString = JsonUtility.ToJson(action);
+            System.IO.File.WriteAllText(filePath, jsonString);
         }
-        string json = JsonConvert.SerializeObject(action.ToArray(), Formatting.Indented);
-        File.WriteAllText(@"/home/anavc/Toxic_Waste_Gym/Unity_Toxic Waste Gym/Assets/state.json", json);*/
+        //string json = JsonConvert.SerializeObject(action.ToArray(), Formatting.Indented);
+        //File.WriteAllText(@"/home/anavc/Toxic_Waste_Gym/Unity_Toxic Waste Gym/Assets/state.json", json);
     }
 }
